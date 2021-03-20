9836accc32c52eb581ff01a609c2fb09151055a0
Firefox0 <you@example.com>
2021-03-20 13:54:06 +0100

    added dash and improved things

--- PlayerController.cs
+++ PlayerController.cs
@@ -1,21 +1,29 @@
 using UnityEngine;
 using UnityEngine.Events;
 
+//TODO: 
+// - air control and walljumping does not work if combined
+// - add attacking (object vanishes if destroyed)
+
 public class PlayerController : MonoBehaviour
 {
 	public float run_speed;
 	public float jump_force;           // Amount of force added when the player jumps.
 	public float horizontal_direction = 0f;
-	const float circle_radius = 0.2f;
+	const float circle_radius = 0.4f;
 	const float movement_smoothing = 0.02f;   // How much to smooth out the movement
-	const float walljump_horizontal_force = 3f;
-	const float walljump_vertical_force = 12f;
+	const float walljump_horizontal_force = 7f;
+	const float walljump_vertical_force = 15f;
+	const float dash_horizontal_force = 70f;
+	const float dash_vertical_force = 0f;
 
 	public bool air_control = false;          // Whether or not a player can steer while jumping;
+	public bool allowed_to_walljump;
 	public bool can_walljump;
 	public bool grounded;                    // Whether or not the player is grounded.
 	public bool jump = false;
 	public bool facing_right = true;          // For determining which way the player is currently facing.
+	public bool dash = false;
 
 	public LayerMask ground_layer;           // A mask determining what is ground to the character
 	public LayerMask wall_layer;
@@ -23,7 +31,6 @@ public class PlayerController : MonoBehaviour
 	public Transform ground_check;            // A position marking where to check if the player is grounded.
 	public Transform ceiling_check;           // A position marking where to check for ceilings
 	public Transform wall_check;
-
 
 	public Rigidbody2D Rigidbody2D;
 	public Vector3 Velocity = Vector3.zero;
@@ -35,10 +42,12 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-		horizontal_direction = Input.GetAxis("Horizontal");
+		horizontal_direction = Input.GetAxisRaw("Horizontal");
 		if (Input.GetKeyDown(KeyCode.Space)) {
-			jump = Input.GetKeyDown(KeyCode.Space);
-        }
+			jump = true;
+        } else if (Input.GetKeyDown(KeyCode.LeftShift)) {
+			dash = true;
+		}
     }
 
     void FixedUpdate()
@@ -47,18 +56,39 @@ public class PlayerController : MonoBehaviour
 		if (grounded && jump) {
 			Jump();
 		}
-		if (horizontal_direction != 0 && (grounded || air_control)) {
+		if (grounded || air_control) {
 			Move(horizontal_direction * run_speed * Time.fixedDeltaTime);
+			if (dash) {
+				Dash(facing_right);
+            }
 		}
-		can_walljump = Physics2D.OverlapCircle(wall_check.position, circle_radius, wall_layer);
-		if (can_walljump && !grounded && jump) {
-			if (facing_right) {
-				Rigidbody2D.velocity = new Vector2(walljump_horizontal_force * -1, walljump_vertical_force);
-			} else {
-				Rigidbody2D.velocity = new Vector2(walljump_horizontal_force, walljump_vertical_force);
+		if (allowed_to_walljump) {
+			can_walljump = Physics2D.OverlapCircle(wall_check.position, circle_radius, wall_layer);
+			if (can_walljump && !grounded && jump) {
+				if (facing_right) {
+					Rigidbody2D.velocity = new Vector2(walljump_horizontal_force * -1, walljump_vertical_force);
+				} else {
+					Rigidbody2D.velocity = new Vector2(walljump_horizontal_force, walljump_vertical_force);
+				}
+				flip();
 			}
-		}
+        }
 		jump = false;
+		dash = false;
+	}
+
+	void Dash(bool right) 
+	{
+		float new_dash_horizontal_force = dash_horizontal_force;
+		float new_dash_vertical_force = dash_vertical_force;
+		if (!grounded) {
+			new_dash_horizontal_force *= 1.5f;
+			new_dash_vertical_force = 5f;
+        }
+		if (!right) {
+			new_dash_horizontal_force *= -1;
+        }
+		Rigidbody2D.velocity = new Vector2(new_dash_horizontal_force, new_dash_vertical_force);
 	}
 
 	void Jump()
@@ -74,12 +104,12 @@ public class PlayerController : MonoBehaviour
 		Rigidbody2D.velocity = Vector3.SmoothDamp(Rigidbody2D.velocity, targetVelocity, ref Velocity, movement_smoothing);
 		if (move > 0 && !facing_right || move < 0 && facing_right) {
 			flip();
-			facing_right = !facing_right;
 		}
 	}
 
 	void flip() 
 	{
+		facing_right = !facing_right;
 		// Multiply the player's x local scale by -1.
 		Vector3 theScale = transform.localScale;
 		theScale.x *= -1;
