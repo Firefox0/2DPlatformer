7e8b5b87b94b193c7dcf0ff1ff2bebba3460cd53
Firefox0 <you@example.com>
2021-03-20 11:54:29 +0100

    first commit

---
+++ PlayerHealth.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public int health = 1;
+
+    public void deal_damage(int damage)
+    {
+        health -= damage;
+        if (health <= 0) {
+            SceneManager.LoadScene("test");
+        }
+    }
+}
