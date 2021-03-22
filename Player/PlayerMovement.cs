1f98a80b2a66180eb9be6ad3898dd7ff2c218fc5
Firefox0 <you@example.com>
2021-03-22 15:45:51 +0100

    added crouching and refactoring

--- Player/PlayerMovement.cs
+++ Player/PlayerMovement.cs
@@ -5,13 +5,11 @@ using System.Collections.Specialized;
 using UnityEngine;
 using UnityEngine.Events;
 
-//TODO: 
-// - add attacking (object vanishes if destroyed)
-// - replace horizontal direction with func for right/left
-
 public class PlayerMovement : MonoBehaviour
 {
+	float current_speed;
 	public float run_speed;
+	public float crouching_factor;
 	public float jump_force;           // Amount of force added when the player jumps.
 	public float horizontal_direction = 0f;
 	const float circle_radius = 0.4f;
@@ -22,9 +20,12 @@ public class PlayerMovement : MonoBehaviour
 	public bool air_control = false;          // Whether or not a player can steer while jumping;
 	public bool allowed_to_walljump;
 	public bool touching_wall;
+	public bool touching_ceiling;
 	public bool grounded;                    // Whether or not the player is grounded.
-	public bool jump = false;
-	public bool shift = false;
+	public bool wants_to_jump = false;
+	public bool wants_to_dash = false;
+	public bool wants_to_crouch = false;
+	public bool crouching = false;
 	public bool dash_ready = true;
 	public bool walljumped = false;
 
@@ -43,16 +44,21 @@ public class PlayerMovement : MonoBehaviour
 	void Awake() {
 		Rigidbody2D = GetComponent<Rigidbody2D>();
 		player_cooldowns = GameObject.Find("Player").GetComponent<PlayerCooldowns>();
+		current_speed = run_speed;
 	}
 
     void Update()
     {
 		horizontal_direction = Input.GetAxisRaw("Horizontal");
 		if (Input.GetKeyDown(KeyCode.Space)) {
-			jump = true;
+			wants_to_jump = true;
 		} else if (Input.GetKeyDown(KeyCode.LeftShift)) {
-			shift = true;
-		}
+			wants_to_dash = true;
+		} else if (Input.GetKeyDown(KeyCode.LeftControl)) {
+			wants_to_crouch = true;
+        } else if (Input.GetKeyUp(KeyCode.LeftControl)) {
+			wants_to_crouch = false;
+        }
 	}
 
     void FixedUpdate()
@@ -60,10 +66,10 @@ public class PlayerMovement : MonoBehaviour
 		// Handle all physics in this special method.
 		grounded = Physics2D.OverlapCircle(ground_check.position, circle_radius, ground_layer);
 		handle_general_movement();
-		if (grounded && jump) {
+		if (grounded && wants_to_jump) {
 			Jump();
 		}
-		if (shift) {
+		if (wants_to_dash) {
 			Dash(facing_right());
 		}
 		if (walljumped && grounded) {
@@ -73,9 +79,31 @@ public class PlayerMovement : MonoBehaviour
 			touching_wall = Physics2D.OverlapCircle(wall_check.position, circle_radius, wall_layer);
 			handle_wall_interactions();
         }
-		jump = false;
-		shift = false;
+		if (wants_to_crouch && !crouching) {
+			Crouch();
+			crouching = true;
+        } else if (!wants_to_crouch && crouching) {
+			touching_ceiling = Physics2D.OverlapCircle(ceiling_check.position, circle_radius, wall_layer);
+			if (!touching_ceiling) {
+				Stand_up();
+				crouching = false;
+            }
+        }
+		wants_to_jump = false;
+		wants_to_dash = false;
 	}
+
+	void Crouch()
+    {
+		current_speed = run_speed * crouching_factor;
+		gameObject.GetComponent<CircleCollider2D>().radius /= 2;
+    }
+
+	void Stand_up()
+    {
+		current_speed = run_speed;
+		gameObject.GetComponent<CircleCollider2D>().radius *= 2;
+    }
 
 	bool facing_right()
     {
@@ -95,7 +123,7 @@ public class PlayerMovement : MonoBehaviour
 		if (walljumped && horizontal_direction == 0) {
 			return;
         }
-		Move(horizontal_direction * run_speed * Time.fixedDeltaTime);
+		Move(horizontal_direction * current_speed * Time.fixedDeltaTime);
 	}
 
 	void handle_wall_interactions()
@@ -124,7 +152,7 @@ public class PlayerMovement : MonoBehaviour
 
 	void walljump(bool right)
     {
-		if (!jump) {
+		if (!wants_to_jump) {
 			return;
         }
 		if (facing_right()) {
