78d5d99fb87319688726c56a0994bfedacbcb6b5
Firefox0 <you@example.com>
2021-03-23 11:44:09 +0100

    added invincibility

--- Enemy/Enemy.cs
+++ Enemy/Enemy.cs
@@ -6,9 +6,14 @@ public class Enemy : MonoBehaviour
 {
 
     public float health;
+    public float damage;
+    public bool invincible;
 
     public void take_damage(float damage)
     {
+        if (invincible) {
+            return;
+        }
         health -= damage;
         if (health <= 0) {
             GameObject.Destroy(gameObject);
