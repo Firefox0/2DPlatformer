26c3056697832ff3ac32104a90007afb81e151b4
Firefox0 <you@example.com>
2021-03-23 11:47:38 +0100

    added collision check

--- Player/PlayerHealth.cs
+++ Player/PlayerHealth.cs
@@ -6,13 +6,37 @@ using UnityEngine.SceneManagement;
 public class PlayerHealth : MonoBehaviour
 {
 
-    public int health;
+    public float health;
+    public Transform character_check;
+    public float character_radius;
+    public LayerMask WhatDealsDamage;
 
-    public void deal_damage(int damage)
+    void Update()
+    {
+        check_collisions();
+    }
+
+    public void take_damage(float damage)
     {
         health -= damage;
         if (health <= 0) {
             SceneManager.LoadScene("test");
         }
+    }
+
+    void check_collisions()
+    {
+        Collider2D collider = Physics2D.OverlapCircle(character_check.position, character_radius, WhatDealsDamage);
+        if (!collider) {
+            return;
+        }
+        float damage = collider.GetComponent<Enemy>().damage;
+        take_damage(damage);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(character_check.position, character_radius);
     }
 }
