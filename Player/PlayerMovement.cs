d519f75e0fe289fa68e91a9d5a78740dea6f0b43
Firefox0 <you@example.com>
2021-03-24 23:46:35 +0100

    refactoring

--- Player/PlayerMovement.cs
+++ Player/PlayerMovement.cs
@@ -10,18 +10,18 @@ public class PlayerMovement : MonoBehaviour
 	float current_speed;
 	public float run_speed;
 	public float crouching_factor;
-	public float jump_force;           // Amount of force added when the player jumps.
+	public float jump_force;     
 	public float horizontal_direction = 0f;
 	const float circle_radius = 0.4f;
 	Vector2 walljump_forces = new Vector2(7f, 15f);
 	Vector2 dash_forces = new Vector2(70f, 0f);
-	const float movement_smoothing = 0.02f;   // How much to smooth out the movement
+	const float movement_smoothing = 0.02f;
 
-	public bool air_control = false;          // Whether or not a player can steer while jumping;
+	public bool air_control = false; 
 	public bool allowed_to_walljump;
 	public bool touching_wall;
 	public bool touching_ceiling;
-	public bool grounded;                    // Whether or not the player is grounded.
+	public bool grounded;                
 	public bool wants_to_jump = false;
 	public bool wants_to_dash = false;
 	public bool wants_to_crouch = false;
@@ -30,177 +30,166 @@ public class PlayerMovement : MonoBehaviour
 	public bool walljumped = false;
 	public bool wallsliding = false;
 
-	public LayerMask ground_layer;           // A mask determining what is ground to the character
+	public LayerMask ground_layer;
 	public LayerMask wall_layer;
 
-	public Transform ground_check;            // A position marking where to check if the player is grounded.
-	public Transform ceiling_check;           // A position marking where to check for ceilings
+	public Transform ground_check;
+	public Transform ceiling_check;
 	public Transform wall_check;
 
-	public Rigidbody2D Rigidbody2D;
+	Rigidbody2D player_rigidbody;
+	CircleCollider2D player_collider;
 	public Vector3 Velocity = Vector3.zero;
 
 	PlayerCooldowns player_cooldowns;
 
 	void Awake() {
-		Rigidbody2D = GetComponent<Rigidbody2D>();
+		player_rigidbody = GetComponent<Rigidbody2D>();
 		player_cooldowns = GameObject.Find("Player").GetComponent<PlayerCooldowns>();
 		current_speed = run_speed;
+		player_collider = GetComponent<CircleCollider2D>();
 	}
 
-    void Update()
-    {
+	void Update()
+	{
 		horizontal_direction = Input.GetAxisRaw("Horizontal");
 		if (Input.GetKeyDown(KeyCode.Space)) {
 			wants_to_jump = true;
-		} else if (Input.GetKeyDown(KeyCode.LeftShift)) {
-			wants_to_dash = true;
-		} else if (Input.GetKeyDown(KeyCode.LeftControl)) {
-			wants_to_crouch = true;
-        } else if (Input.GetKeyUp(KeyCode.LeftControl)) {
-			wants_to_crouch = false;
         }
+		if (Input.GetKeyDown(KeyCode.LeftShift)) {
+			wants_to_dash = true;
+        }
+		wants_to_crouch = Input.GetKey(KeyCode.LeftControl);
 	}
 
-    void FixedUpdate()
+	void FixedUpdate()
 	{
 		// Handle all physics in this special method.
+		
+		handle_movements();
+	}
+
+	void handle_movements() {
 		grounded = Physics2D.OverlapCircle(ground_check.position, circle_radius, ground_layer);
-		handle_general_movement();
+
+		// Left and right movement. Don't slow down after walljumping.
+		if (grounded || air_control &&
+			!walljumped || horizontal_direction != 0) {
+			move(horizontal_direction * current_speed);;
+		}
+
 		if (grounded && wants_to_jump) {
-			Jump();
+			jump();
 		}
 		if (wants_to_dash) {
-			Dash(facing_right());
+			dash(facing_right());
 		}
+
 		if (walljumped && grounded) {
 			walljumped = false;
-        }
-		if (allowed_to_walljump) {
-			touching_wall = Physics2D.OverlapCircle(wall_check.position, circle_radius, wall_layer);
-			handle_wall_interactions();
-        }
+		}
+		handle_wall_interactions();
+
 		if (wants_to_crouch && !crouching) {
-			Crouch();
+			crouch();
 			crouching = true;
-        } else if (!wants_to_crouch && crouching) {
+		} else if (!wants_to_crouch && crouching) {
 			touching_ceiling = Physics2D.OverlapCircle(ceiling_check.position, circle_radius, wall_layer);
 			if (!touching_ceiling) {
-				Stand_up();
+				stand_up();
 				crouching = false;
-            }
-        }
+			}
+		}
+
 		wants_to_jump = false;
 		wants_to_dash = false;
 	}
 
-	void Crouch()
-    {
+	void crouch()
+	{
 		current_speed = run_speed * crouching_factor;
-		gameObject.GetComponent<CircleCollider2D>().radius /= 2;
-    }
+		player_collider.radius /= 2;
+	}
 
-	void Stand_up()
-    {
+	void stand_up()
+	{
 		current_speed = run_speed;
-		gameObject.GetComponent<CircleCollider2D>().radius *= 2;
-    }
+		player_collider.radius *= 2;
+	}
 
 	bool facing_right()
-    {
+	{
 		if (transform.localScale.x == -1) {
 			return false;
-        }
+		}
 		return true;
-    }
-
-	void handle_general_movement()
-    {
-		// Left and right movements.
-		if (!grounded && !air_control) {
-			return;
-        }
-		// Don't slow down after walljumping.
-		if (walljumped && horizontal_direction == 0) {
-			return;
-        }
-		Move(horizontal_direction * current_speed * Time.fixedDeltaTime);
 	}
 
 	void handle_wall_interactions()
-    {
+	{
+		touching_wall = Physics2D.OverlapCircle(wall_check.position, circle_radius, wall_layer);
 		if (!touching_wall || grounded) {
-			Rigidbody2D.gravityScale = 3f;
+			player_rigidbody.gravityScale = 3f;
 			wallsliding = false;
 			return;
-        }
+		}
 		wallslide();
-		walljump(facing_right());
+		if (wants_to_jump) {
+			walljump(facing_right());
+		}
 	}
 
 	void wallslide()
-    {
-		if (wallsliding) {
+	{
+		if (wallsliding ||
+			player_rigidbody.velocity.y >= 0 || 
+			!facing_right() && horizontal_direction != -1 ||
+			facing_right() && horizontal_direction != 1) {
 			return;
-        }
-		if (Rigidbody2D.velocity.y >= 0) {
-			return;
-        }
-		if (!facing_right() && horizontal_direction != -1 || facing_right() && horizontal_direction != 1) {
-			return;
-        }
-		Rigidbody2D.gravityScale = 0.25f;
-		Rigidbody2D.velocity = new Vector2(0, 0);
+		}
+		player_rigidbody.gravityScale = 0.25f;
+		player_rigidbody.velocity = new Vector2(0, 0);
 		wallsliding = true;
 	}
 
 	void walljump(bool right)
-    {
-		if (!wants_to_jump) {
+	{
+		if (!allowed_to_walljump) {
 			return;
-        }
+		}
 		if (facing_right()) {
-			Rigidbody2D.velocity = new Vector2(walljump_forces.x * -1, walljump_forces.y);
+			player_rigidbody.velocity = new Vector2(walljump_forces.x * -1, walljump_forces.y);
 		} else {
-			Rigidbody2D.velocity = walljump_forces;
+			player_rigidbody.velocity = walljump_forces;
 		}
 		flip();
 		walljumped = true;
 	}
 
-	void Dash(bool right)
+	void dash(bool right)
 	{
 		if (!player_cooldowns.check_cooldown("Dash")) {
 			return;
 		}
 		Vector2 new_dash_forces = dash_forces;
-		// Add more force while airborne to compensate for gravity.
-		if (!grounded) {
-			new_dash_forces.x *= 1.5f;
-			new_dash_forces.y = 5f;
-		}
 		if (!right) {
 			new_dash_forces.x *= -1;
 		}
-		// TODO: Fix super dash after walljumping properly.
-		if (walljumped) {
-			new_dash_forces.x /= 7;
-		}
-		Rigidbody2D.velocity = new_dash_forces;
+		player_rigidbody.velocity = new_dash_forces;
 	}
 
-	void Jump()
-    {
-		Rigidbody2D.AddForce(new Vector2(0f, jump_force));
+	void jump()
+	{
+		player_rigidbody.AddForce(new Vector2(0f, jump_force));
 	}
 
-	void Move(float move)
+	void move(float speed)
 	{
 		// Move the character by finding the target velocity.
-		Vector3 targetVelocity = new Vector2(move * 10f, Rigidbody2D.velocity.y);
+		Vector3 targetVelocity = new Vector2(speed, player_rigidbody.velocity.y);
 		// And then smoothing it out and applying it to the character.
-		Rigidbody2D.velocity = Vector3.SmoothDamp(Rigidbody2D.velocity, targetVelocity, ref Velocity, movement_smoothing);
-		if (move > 0 && !facing_right() || move < 0 && facing_right()) {
+		player_rigidbody.velocity = Vector3.SmoothDamp(player_rigidbody.velocity, targetVelocity, ref Velocity, movement_smoothing);
+		if (speed > 0 && !facing_right() || speed < 0 && facing_right()) {
 			flip();
 		}
 	}
